7fc5b50539840fea819ca25078c5dc6fa27d1981
Firas Moosvi <firas@moosvi.com>
2023-03-16 00:16:46 -0400

    update feedback

--- questions/tour/rankQuestionTypes/server.py
+++ questions/tour/rankQuestionTypes/server.py
@@ -3,10 +3,18 @@ def grade(data):
     if len(data['submitted_answers']['questions']) == 5:
         # This will give participants a mark regardless of what answers they select!
         data['score'] = 1
+
+        data["feedback"] = "Great, thank you so much for engaging with this activity!"
+
     
     elif len(data['submitted_answers']['questions']) < 5:
         data['format_errors']['question'] = "Please select at least 5 choices!"
+
+        data["feedback"] = data['format_errors']['question']
         
     elif len(data['submitted_answers']['questions']) > 5:
         data['format_errors']['question'] = "Oops! You selected too many choices, please select only 5 choices!"
+
+        data["feedback"] = data['format_errors']['question']
+
     
