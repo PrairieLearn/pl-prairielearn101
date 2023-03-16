528854fe235cbfa66fc2584a8de6ce098d9a9d9b
Firas Moosvi <firas.moosvi@ubc.ca>
2023-03-16 03:58:40 +0000

    rankQuestionTypes: edit server.py

--- questions/tour/rankQuestionTypes/server.py
+++ questions/tour/rankQuestionTypes/server.py
@@ -1,4 +1,7 @@
 def grade(data):
+    
+    #if data['submitted_answers'] == 
+    print(data['submitted_answers'])
 
     # This will give participants a mark regardless of what answers they select!
     data['score'] = 1
