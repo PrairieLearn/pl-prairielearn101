5f9eb2a6182558d211f6472ea6e185ecad243454
Firas Moosvi <firas@moosvi.com>
2023-03-16 02:18:23 -0400

    comment out some code

--- questions/tour/rankQuestionTypes/server.py
+++ questions/tour/rankQuestionTypes/server.py
@@ -3,6 +3,9 @@ def grade(data):
     if len(data['submitted_answers']['questions']) == 5:
         # This will give participants a mark regardless of what answers they select!
         data['score'] = 1
+
+        # To get rid of the red 0% badge (since we're marking everything right),
+        # We need to over-write the partial_scores dict
         data['partial_scores'] = {}
 
     elif len(data['submitted_answers']['questions']) < 5:
@@ -10,7 +13,3 @@ def grade(data):
         
     elif len(data['submitted_answers']['questions']) > 5:
         data['format_errors']['questions'] = {"invalid":"Oops! You selected too many choices, please select only 5 choices!"}
-
-    # if data["score"] == 1:
-
-    #print(data)
