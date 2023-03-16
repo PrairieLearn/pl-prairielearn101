1166fd14b2f7b1c9047bd90646f73579bcbdfccf
Firas Moosvi <firas.moosvi@ubc.ca>
2023-03-16 04:05:08 +0000

    rankQuestionTypes: edit server.py

--- questions/tour/rankQuestionTypes/server.py
+++ questions/tour/rankQuestionTypes/server.py
@@ -1,13 +1,13 @@
 def grade(data):
     
-    if len(dic['questions']) == 5:
+    if len(data['submitted_answer']['questions']) == 5:
         # This will give participants a mark regardless of what answers they select!
         data['score'] = 1
     
-    elif len(dic['questions']) < 5:
+    elif len(data['submitted_answer']['questions']) < 5:
         data['format_errors'] = "Please select at least 5 choices!"
         
-    elif len(dic['questions']) > 5:
+    elif len(data['submitted_answer']['questions']) > 5:
         data['format_errors'] = "Oops! You selected too many choices, please select only 5 choices!"
 
     
