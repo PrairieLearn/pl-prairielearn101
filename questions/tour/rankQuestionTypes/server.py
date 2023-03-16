c4a3995eef3bf5272178ce26f0a4b1e5e77b9c73
Firas Moosvi <firas.moosvi@ubc.ca>
2023-03-16 05:11:00 +0000

    rankQuestionTypes: edit server.py

--- questions/tour/rankQuestionTypes/server.py
+++ questions/tour/rankQuestionTypes/server.py
@@ -5,10 +5,10 @@ def grade(data):
         data['score'] = 1
 
     elif len(data['submitted_answers']['questions']) < 5:
-        data['format_errors']['questions'] = {"Please select at least 5 choices!"}
+        data['format_errors']['questions'] = {"invalid":"Please select at least 5 choices!"}
         
     elif len(data['submitted_answers']['questions']) > 5:
-        data['format_errors']['questions'] = {"Oops! You selected too many choices, please select only 5 choices!"}
+        data['format_errors']['questions'] = {"invalid":"Oops! You selected too many choices, please select only 5 choices!"}
 
     # if data["score"] == 1:
 
