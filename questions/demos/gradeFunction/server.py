3dc61e265fc7ef1dcb9b28a5a14a5f56d6d0cf61
Firas Moosvi <firas@moosvi.com>
2023-03-17 05:19:15 -0400

    update feedback to be in hint

--- questions/demos/gradeFunction/server.py
+++ questions/demos/gradeFunction/server.py
@@ -23,12 +23,8 @@ def grade(data):
         # get the submitted answer, defaulting to empty string if it's missing
         sub = data["submitted_answers"].get("c", "")
         if len(sub.replace("0", "").replace("1", "")) > 0:
-            data["feedback"][
-                "c"
-            ] = "Your answer should not contain characters other than '0' and '1'"
+            data["feedback"]["c"] = "Your answer should not contain characters other than '0' and '1'"
         elif len(sub) != len(data["correct_answers"]["c"]):
             data["feedback"]["c"] = "Your answer has the wrong length"
         else:
-            data["feedback"][
-                "c"
-            ] = "Your answer was has the correct length and format, but the value is wrong"
+            data["feedback"]["c"] = "Your answer was has the correct length and format, but the value is wrong"
