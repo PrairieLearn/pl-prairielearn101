771f3f5214c811f66d2e87b8dbeeb39a31c9e59f
Firas Moosvi <firas.moosvi@ubc.ca>
2023-03-18 18:49:49 +0000

    myspecialquestion: edit server.py

--- questions/tour/myspecialquestion/server.py
+++ questions/tour/myspecialquestion/server.py
@@ -4,3 +4,4 @@ import prairielearn as pl
 def generate(data):
     
     data['params']['random_number'] = random.randint(0,500)
+    data['params']['random_number_hint'] = data['params']['random_number']  / 2
