37448b845ea57a1ff25eef3dc24c85bacb583ddc
Craig Zilles <zilles@illinois.edu>
2023-07-21 17:03:17 +0000

    pulleyHorizontalPlane: edit server.py

--- questions/pulleyHorizontalPlane/server.py
+++ questions/pulleyHorizontalPlane/server.py
@@ -2,6 +2,18 @@ import random
 
 
 def generate(data):
+    g = 10
     ma = random.randint(4, 8)
+    while (True):
+        mb = random.randint(4, 8)
+        if ma != mb:
+            break
+        
     data["params"]["ma"] = ma
-    data["correct_answers"]["F"] = ma * 10
+    data["params"]["mb"] = mb
+    data["params"]["g"] = g
+    
+    net_force = (mb - ma) * g
+    combined_mass = ma + mb
+    acceleration = net_force / combined_mass
+    data["correct_answers"]["a"] = acceleration
