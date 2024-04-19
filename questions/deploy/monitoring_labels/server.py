acd6550ac7b2297b452b9d5fc29e85348bebcaab
Fraida Fund <ff524@nyu.edu>
2024-04-19 16:28:00 +0000

    monitoring_labels: modify server.py

--- questions/deploy/monitoring_labels/server.py
+++ questions/deploy/monitoring_labels/server.py
@@ -2,16 +2,8 @@ import random
 
 
 def generate(data):
-    # Sample two random integers between 5 and 10 (inclusive)
-    a = random.randint(5, 10)
-    b = random.randint(5, 10)
-
-    # Put these two integers into data['params']
-    data["params"]["a"] = a
-    data["params"]["b"] = b
-
-    # Compute the sum of these two integers
-    c = a + b
-
-    # Put the sum into data['correct_answers']
-    data["correct_answers"]["c"] = c
+    data['params'] = random.choice([
+        {'scenario': "translate user-supplied text from one language to another", 'approach': "ask users for explicit feedback on each translation (thumbs up or thumbs down)",
+        'correct': ["The response rate is likely to be low", "The user may not be a reliable judge of whether the prediction is good or not"],
+        'incorrect': [""]
+        ])
