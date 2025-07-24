22c13e1f0bbf03f13bd7ee06a656404410b76656
Steven Wolfman <wolf@mail.ubc.ca>
2025-07-24 13:32:07 -0700

    utility questions to share from CPSC 121

---
+++ questions/steve/ubc_cpsc_121/Notes_Area/server.py
@@ -0,0 +1,7 @@
+# This problem was developed by Steven Wolfman at UBC, 2025.
+# It is available for use via a CC-BY-4.0 license: https://creativecommons.org/licenses/by/4.0/.
+import random
+
+def grade(data):
+    data["format_errors"]["scratch-notes"] = "This question cannot be graded. Use the 'Save only' button to save your scratch work, not 'Save & Grade'."
+    data["score"] = 0
